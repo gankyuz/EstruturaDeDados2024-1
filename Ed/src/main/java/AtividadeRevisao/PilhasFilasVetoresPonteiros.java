5a1989525e74a4e909ec4197a0664cb18aa47ab8
gabriela.oliveira <gabisoliporto@gmail.com>
2024-08-19 11:35:32 -0300

    att

--- Ed/src/main/java/AtividadeRevisao/PilhasFilasVetoresPonteiros.java
+++ Ed/src/main/java/AtividadeRevisao/PilhasFilasVetoresPonteiros.java
@@ -129,6 +129,104 @@ public class PilhasFilasVetoresPonteiros {
             }
         }
     }
+    public class FilaComRedimensionamento{
+        private int[] elementos;
+        private int inicio, fim;
+        private int capacidade;
+        public FilaComRedimensionamento(int capacidadeInicial){
+            this.elementos = new int[capacidadeInicial];
+            this.inicio = 0;
+            this.fim = -1;
+            this.capacidade = capacidadeInicial;
+
+        }
+        //7. Descreva o algoritmo(ou código) que insira um elemento em uma fila com vetores que
+        //permita redimensionamento.
+        public void enfileirar(int valor){
+            if (capacidade == -1){
+                redimensionar();
+            }
+            fim++;
+            elementos[fim] = valor;
+        }
+        public void redimensionar(){
+            int[] novoVetor = new int[capacidade*2];
+            int index = 0;
+            for (int i = inicio; i<=fim;i++){
+                novoVetor[index++] = elementos[i];
+            }
+            elementos = novoVetor;
+            inicio = 0;
+            fim = index-1;
+            capacidade *= 2;
+        }
+        //8. Descreva o algoritmo(ou código) que remova um elemento em uma fila com vetores que
+        //permita redimensionamento.
+
+        public int desinfileirar(){
+            if (isEmpty()){
+                System.out.println("Fila Vazia");
+            }
+            int valor = elementos[inicio];
+            inicio++;
+            //verificar se a fila está quase vazia e se vale a pena redimensionar
+            if (inicio == capacidade/4){
+                redimensionar();
+            }
+            return valor;
+        }
+        public boolean isEmpty(){
+            return (inicio > fim);
+        }
+    }
+    public class FilaComPonteiros{
+        public class Node{
+            private int data;
+            private Node next;
+            public Node(int data){
+                this.data = data;
+                this.next = null;
+            }
+        }
+        public class Fila{
+            private Node front, rear;
+            public Fila(){
+                this.front = this.rear = null;
+            }
+            //9. Descreva o algoritmo(ou código) que insira um elemento em uma fila com ponteiros (lista
+            //simplesmente encadeada).
+            public void enqueue(int x){
+                Node temp = new Node(x);
+                // se a fila estiver vazia, o novo nó se forma tanto front quanto rear
+                if (this.rear == null){
+                    this.front = this.rear = temp;
+                    return;
+                }
+                //adiciona o novo nó ao final da fila
+                this.rear.next = temp;
+                this.rear = temp;
+            }
+            //10. Descreva o algoritmo(ou código) que remova um elemento em uma fila com ponteiros
+            //(lista simplesmente encadeada).
+
+            public int dequeue(){
+                if(isEmpty()){
+                    System.out.println("Fila Vazia");
+                    return -1;
+                }
+                int data = front.data;
+                front = front.next;
+                //se a fila ficar vazia, o rear também aponta para null
+                if (front == null) {
+                    rear = null;
+                }
+                return data;
+            }
+            public boolean isEmpty(){
+                return front == null;
+            }
+        }
+    }
 
 
 }
